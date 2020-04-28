9ec2e05a1af122463118e458addd0f27e8454db9
designed4device <designed4device@gmail.com>
2020-04-28 09:45:01 -0500

    null checks

--- plugin.js
+++ plugin.js
@@ -2,11 +2,11 @@ module.exports.requestHooks = [
   context => {
     let url = context.request.getUrl();
     let templateMatch = url.match(/\{([?&])(.*)\}/);
-    if (templateMatch.length > 0) {
+    if (templateMatch != null && templateMatch.length > 0) {
       let templateParams = templateMatch[2].split(",");
       if (templateMatch[1] == "?" || templateMatch[1] == "&") {
         let unhandled = templateParams.filter(it => !context.request.hasParameter(it));
-        if (unhandled.length > 0) {
+        if (unhandled != null && unhandled.length > 0) {
           context.app.alert(
             "Warning: Unhandled URI Template Param",
             "The request uri contains unhandled template params: " + unhandled.join()
