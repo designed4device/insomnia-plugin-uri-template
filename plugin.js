b62a3f0b016ce205eaa94566b966dccf89b097b9
designed4device <designed4device@gmail.com>
2020-04-27 23:37:47 -0500

    initial commit; working plugin with support for query param templates

---
+++ plugin.js
@@ -0,0 +1,19 @@
+module.exports.requestHooks = [
+  context => {
+    let url = context.request.getUrl();
+    let templateMatch = url.match(/\{([?&])(.*)\}/);
+    if (templateMatch.length > 0) {
+      let templateParams = templateMatch[2].split(",");
+      if (templateMatch[1] == "?" || templateMatch[1] == "&") {
+        let unhandled = templateParams.filter(it => !context.request.hasParameter(it));
+        if (unhandled.length > 0) {
+          context.app.alert(
+            "Warning: Unhandled URI Template Param",
+            "The request uri contains unhandled template params: " + unhandled.join()
+          );
+        }
+        context.request.setUrl(url.replace(templateMatch[0], ""));
+      }
+    }
+  }
+];
